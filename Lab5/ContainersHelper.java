84ca24564b81f01418e1bf5015e695204c846750
BohdanHornostai <43396599+BohdanHornostai@users.noreply.github.com>
2019-06-01 16:41:53 +0300

    Add files via upload

---
+++ Lab5/ContainersHelper.java
@@ -0,0 +1,21 @@
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+
+public class ContainersHelper {
+
+    public static boolean isValidName(String name) {
+        Pattern pattern = Pattern.compile(".+ \\(.+\\)");
+        return pattern.matcher(name).find();
+    }
+
+
+    public static Path isValidFormat() {
+    	Path p = PathGetter.getPath(Pattern.compile(".+\\.xml$"));    	
+        return p;
+    }
+    
+    public static Path isValidF() {
+    	Path p = PathGetter.getPath(Pattern.compile("[^.]+(\\.[^.]+)?"));    	
+        return p;
+    }
+}
